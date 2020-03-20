29abd651b9300e4752ddf814de605f650beab84b
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-20 22:15:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -74,7 +74,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
- programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages ["LISP", "C"]}
+ programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
  end
  programmer_hash
 end
